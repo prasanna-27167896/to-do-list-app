2a0c4460f1c7e29fe61c13ec667e8c9c9fbba0d9
prasanna kumar <vprasannakumar2716@gmail.com>
2025-03-10 14:30:18 +0530

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputEl = document.getElementById("input");
 const buttonEl = document.getElementById("btn");
 const ul = document.getElementById("list");
+const para = document.querySelector(".para");
 
 function addTask() {
   if (!inputEl.value.trim()) {
@@ -29,3 +30,55 @@ ul.querySelectorAll("li button").forEach((button) => {
 });
 
 buttonEl.addEventListener("click", addTask);
+
+///////////////////////////////////
+
+///h/w
+
+function renderHtml(data) {
+  try {
+    let html = `<button class="btn2">🧹</button> <span>${data.title}</span>`;
+    console.log(data.title);
+
+    para.insertAdjacentHTML("beforeend", html);
+
+    document.querySelectorAll(".btn2").forEach((btn) => {
+      btn.addEventListener("click", function () {
+        btn.parentElement.remove();
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//Usual Method
+
+// function getTaskData(task) {
+//   fetch(
+//     `https://jsonplaceholder.typicode.com/todos/${
+//       Math.floor(Math.random() * 100) + 1
+//     }`
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err));
+// }
+// getTaskData();
+
+const getTaskData = async function (task) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${task}`
+  );
+  const data = await response.json();
+  console.log(data);
+  console.log(data.title);
+
+  renderHtml(data);
+};
+
+let task = Math.floor(Math.random() * 200) + 1;
+
+getTaskData(task);
